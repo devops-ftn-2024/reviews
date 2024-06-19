97ad32d66a476c12e4aa2fcb0c9f7d0bb29f662a
Aleksandra Nedic <nedicaleksandra843@gmail.com>
2024-06-20 00:04:09 +0200

    chore: add tracing logging and monitoring

--- src/util/validation.ts
+++ src/util/validation.ts
@@ -1,23 +1,30 @@
 import { BadRequestError } from "../types/errors";
 import { Review, Type } from "../types/reviews";
+import { Logger } from "./logger";
 
 export const validateReview = (review: Review) => {
     if (!review) {
+        Logger.error("Missing review parameter");
         throw new BadRequestError("Missing review parameter");
     }
     if (!review.type) {
+        Logger.error("Missing review type parameter");
         throw new BadRequestError("Missing review type parameter");
     }
     if (review.type === Type.ACCOMMODATION && !review.entityId) {
+        Logger.error("Missing review entityId parameter");
         throw new BadRequestError("Missing review entityId parameter");
     }
     if (review.type === Type.HOST && !review.hostUsername) {
+        Logger.error("Missing review hostUsername parameter");
         throw new BadRequestError("Missing review hostUsername parameter");
     }
     if (!review.rating) {
+        Logger.error("Missing review rating parameter");
         throw new BadRequestError("Missing review rating parameter");
     }
     if (review.rating < 1 || review.rating > 5) {
+        Logger.error("Invalid review rating parameter");
         throw new BadRequestError("Invalid review rating parameter");
     }
 };
